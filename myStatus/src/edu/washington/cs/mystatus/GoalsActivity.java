503563e4a52e55beffe92c5974f675a7ca3fa8f8
Emily Chien <eechien@uw.edu>
2013-04-16 14:24:03 -0700

    Added Buttons and Weight to layout

--- myStatus/src/edu/washington/cs/mystatus/GoalsActivity.java
+++ myStatus/src/edu/washington/cs/mystatus/GoalsActivity.java
@@ -3,6 +3,8 @@ package edu.washington.cs.mystatus;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.Spinner;
 
 /**
  * GoalsActivity provides a UI for setting new goals and seeing active goals.
@@ -12,17 +14,14 @@ import android.view.Menu;
 
 public class GoalsActivity extends Activity {
 
+	private Spinner newGoal, removeGoal;
+	private Button newGoalButton, removeGoalButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_goals);
+		
+		
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_goals, menu);
-		return true;
-	}
-
 }
