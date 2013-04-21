f6600f3a14022c3eeab6daa4ffad5c1ea2ebeda9
Jake Bailey <rjacob@cs.washington.edu>
2013-04-20 22:12:32 -0700

    Add contact picker for the Call a Friend button in the Get Help activity.

--- myStatus/src/edu/washington/cs/mystatus/HelpActivity.java
+++ myStatus/src/edu/washington/cs/mystatus/HelpActivity.java
@@ -2,10 +2,15 @@ package edu.washington.cs.mystatus;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
+import android.provider.ContactsContract.Contacts;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * HelpActivity provides quick-dial buttons for important contacts. Eventually,
@@ -16,6 +21,10 @@ import android.widget.Button;
  */
 public class HelpActivity extends Activity {
 
+	private static final String TAG = "mystatus.HelpActivity";
+
+	private static final int PICK_CONTACT_REQUEST = 0;
+
 	private static final String DOCTOR_NUM = "tel:555-1234";
 	private static final String EMERGENCY_NUM = "tel:911";
 	private static final String LIFELINE_NUM = "tel:1-800-273-8255"; // National Suicide Prevention Lifeline
@@ -24,6 +33,7 @@ public class HelpActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_help);
+		Log.d(TAG, "Help activity created.");
 
 		Button callDoctor = (Button) findViewById(R.id.call_doctor);
 		Button callEmergencyServices = (Button) findViewById(R.id.call_emergency_services);
@@ -54,7 +64,9 @@ public class HelpActivity extends Activity {
 		callFriend.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-
+				Intent pickContactIntent = new Intent(Intent.ACTION_PICK, Contacts.CONTENT_URI);
+				pickContactIntent.setType(Phone.CONTENT_TYPE); // only contacts w/ phone numbers
+				startActivityForResult(pickContactIntent, PICK_CONTACT_REQUEST);
 			}
 		});
 	}
@@ -67,5 +79,30 @@ public class HelpActivity extends Activity {
 	private void dialNumber(Uri phoneNumber) {
 		Intent dialIntent = new Intent(Intent.ACTION_DIAL).setData(phoneNumber);
 		startActivity(dialIntent);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch (requestCode) {
+		case PICK_CONTACT_REQUEST:
+			if (resultCode == RESULT_OK) {
+				Uri contactUri = data.getData();
+				String[] projection = { Phone.NUMBER };
+
+				Cursor cursor = getContentResolver()
+						.query(contactUri, projection, null, null, null);
+				cursor.moveToFirst();
+
+				// Retrieve the phone number from the NUMBER column
+				int column = cursor.getColumnIndex(Phone.NUMBER);
+				String number = cursor.getString(column);
+				cursor.close();
+
+				dialNumber(Uri.parse("tel:" + number));
+			} else {
+				Toast.makeText(this, "Couldn't get phone number for contact.", Toast.LENGTH_SHORT)
+						.show();
+			}
+		}
 	}
 }
