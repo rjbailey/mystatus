0ecd6686a8d702b7b45791828569a1866dd87038
Jake Bailey <rjacob@cs.washington.edu>
2013-06-04 22:14:39 -0700

    Add delayed and once-only survey predicates.

--- myStatus/src/edu/washington/cs/mystatus/utilities/PredicateSolver.java
+++ myStatus/src/edu/washington/cs/mystatus/utilities/PredicateSolver.java
@@ -29,7 +29,8 @@ public class PredicateSolver {
                 FormsColumns._ID,
                 FormsColumns.DISPLAY_NAME,
                 FormsColumns.PREDICATE,
-                FormsColumns.LAST_RESPONSE
+                FormsColumns.LAST_RESPONSE,
+                FormsColumns.DATE
         };
         String selection = FormsColumns.NEEDS_RESPONSE + " = 0 AND "
                 + FormsColumns.FORM_TYPE + " = ?";
@@ -56,10 +57,20 @@ public class PredicateSolver {
     /**
      * Evaluates a single predicate.
      * <p>
-     * For now, a predicate is simply a Long value representing the number of
-     * milliseconds that should have elapsed since the last response to the
-     * survey.
-     * <p>
+     * Valid predicates include:
+     * <ul>
+     * <li>The number of milliseconds that need to have elapsed since the last
+     * response to the survey.</li>
+     * <li>The string "daysSinceLastResponse:N", where N is the number of days
+     * that need to have elapsed since the last response to the survey.</li>
+     * <li>The string "onceOnly", which indicates that the survey only ever
+     * needs a single response, and will not be displayed after the user has
+     * responded to it.</li>
+     * <li>The string "daysDelayed:N", where N is the number of days that need
+     * to have elapsed since the survey was downloaded before it is made visible
+     * to the user. Delayed surveys currently only ever accept one response
+     * (like onceOnly surveys).</li>
+     * </ul>
      * Returns true for invalid predicates, but this may change in the future.
      * We should return true for null predicates (because those represent
      * unconditional surveys that may be responded to at any time), but when the
@@ -67,7 +78,7 @@ public class PredicateSolver {
      * to do something else.
      * 
      * @param c A Cursor positioned on a row with projection (id, name,
-     *        predicate, lastResponse).
+     *        predicate, lastResponse, date).
      * @return If the predicate and its input values (e.g. lastResponse) are
      *         valid, returns the truth value of the evaluated predicate. If the
      *         predicate or its input values are invalid, returns true.
@@ -77,13 +88,45 @@ public class PredicateSolver {
         String formName = c.getString(1);
         Log.d(TAG, "Evaluating predicate for Form #" + formId + ": " + formName);
 
-        if (c.isNull(2) || c.isNull(3)) {
-            if (c.isNull(2)) Log.i(TAG, "Predicate is null.");
-            if (c.isNull(3)) Log.i(TAG, "The survey has never been filled out.");
+        if (c.isNull(2)) {
+            Log.i(TAG, "Predicate is null.");
             return true;
         }
 
         String predicate = c.getString(2);
+
+        if (c.isNull(3)) {
+            Log.i(TAG, "The survey has never been filled out.");
+
+            if (predicate.contains("daysDelayed:")) {
+                // Delayed surveys only need a response if it's been at least
+                // N days since the survey was downloaded.
+
+                Long now = Long.valueOf(System.currentTimeMillis());
+                Long subscriptionDate = c.getLong(4);
+
+                long daysToDelay;
+                try {
+                    daysToDelay = Long.valueOf(predicate.split(":")[1]);
+                } catch (NumberFormatException e) {
+                    Log.e(TAG, "Invalid predicate: \"" + predicate + "\"");
+                    return true;
+                }
+
+                long millisPerDay = 24 * 60 * 60 * 1000;
+                long daysElapsed = (now - subscriptionDate) / millisPerDay;
+
+                return daysElapsed >= daysToDelay;
+            }
+            // All other surveys need a response if they've never had one.
+            return true;
+        }
+
+        if (predicate.equals("onceOnly")) {
+            Log.i(TAG, "The survey is a once-only survey which has been filled out.");
+            return false;
+        }
+
         Long lastResponseTime = c.getLong(3);
 
         Long waitTime;
