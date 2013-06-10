6449d5e56eeb47671764d8750a3f0e44b526ccbf
Emily Chien <eechien@uw.edu>
2013-06-09 17:06:14 -0700

    Notifications now are snoozed when the snooze button is pressed

--- myStatus/src/edu/washington/cs/mystatus/services/NotificationService.java
+++ myStatus/src/edu/washington/cs/mystatus/services/NotificationService.java
@@ -7,6 +7,7 @@ import edu.washington.cs.mystatus.application.MyStatus;
 import edu.washington.cs.mystatus.odk.provider.FormsProviderAPI.FormTypes;
 import edu.washington.cs.mystatus.odk.provider.FormsProviderAPI.FormsColumns;
 import edu.washington.cs.mystatus.utilities.PredicateSolver;
+import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -14,7 +15,6 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.media.RingtoneManager;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
@@ -32,33 +32,34 @@ public class NotificationService extends Service{
 	private static final String TAG = "NotificationService";
 
 	private static final int NOTIFICATION_ID = 0;
-
+	private static final int NOTIFICATION_ID_SNOOZE = 1;
+	private static int MILLIS = 300000; // 5 minutes
+	
 	private static final String ACTION_DISMISS = "dismiss";
 	private static final String ACTION_SNOOZE = "snooze";
-
+	private NotificationManager mNotificationManager;
+	NotificationCompat.Builder builder;
+	
 	@Override
-	public IBinder onBind(Intent intent) {
+	public IBinder onBind(Intent arg0) {
 		return null;
 	}
-
+	
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		Log.d(TAG, "NotificationService started.");
-
 		String action = intent.getAction();
+		NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 		if (action == null) {
 			Log.d(TAG, "Notifying if needed.");
 			notifyIfNeeded();
 		} else if (action.equals(ACTION_DISMISS)) {
 			Log.d(TAG, "Dismissing notification.");
-			NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 			nm.cancel(NOTIFICATION_ID);
 		} else if (action.equals(ACTION_SNOOZE)) {
-			// TODO: Support snooze for notifications
-			Log.d(TAG, "Snooze requested--dismissing notification instead.");
-			NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+			Log.d(TAG, "Snoozeing notification.");
 			nm.cancel(NOTIFICATION_ID);
-			
+			startSnooze();
 		}
 		// fixed this for preventing services from restart
 		return super.onStartCommand(intent, flags, startId);
@@ -82,21 +83,16 @@ public class NotificationService extends Service{
 		// only generate a notification if there's at least one survey to respond to
 		if (c.getCount() > 0) {
 			Log.i(TAG, c.getCount() + " survey(s) need a response. Generating notification...");
-			Notification surveyNotification = createSurveyNotification();
-			surveyNotification.flags = Notification.DEFAULT_LIGHTS | Notification.FLAG_AUTO_CANCEL;
-			NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-			nm.notify(NOTIFICATION_ID, surveyNotification);
+			issueNotification();
 		} else {
 			Log.i(TAG, "No surveys need a response, so no notification was generated.");
 		}
 		c.close();
 	}
-
-	/**
-	 * @return A Notification which launches the surveys list when clicked.
-	 */
-	private Notification createSurveyNotification() {
-		// Create a PendingIntent that will launch an ODK Collect survey
+	
+	// creates a notification with snooze and dismiss buttons
+	private void issueNotification() {
+		mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 		PendingIntent surveyIntent = PendingIntent.getActivity(this, 0,
 				new Intent(this, SurveyListTabs.class), Intent.FLAG_ACTIVITY_NEW_TASK);
 		
@@ -111,22 +107,32 @@ public class NotificationService extends Service{
 		snoozeIntent.setAction(ACTION_SNOOZE);
 		PendingIntent piSnooze = PendingIntent.getService(this, 0, snoozeIntent, 0);
 
-		NotificationCompat.Builder nb = new NotificationCompat.Builder(this)
+		builder = new NotificationCompat.Builder(this)
 				.setSmallIcon(android.R.drawable.ic_menu_my_calendar)
 				.setContentTitle(getResources().getText(R.string.notification_title))
 				.setContentText(getResources().getText(R.string.notification_message))
 				.setContentIntent(surveyIntent)
 				.setDefaults(Notification.DEFAULT_ALL)
-				//.setAutoCancel(true)
-
+				.setAutoCancel(true)
+				
+				// for devices Android 4.1 and later
 				.setStyle(bigViewStyle
 						.bigText(getResources().getText(R.string.notification_message)))
 				.addAction(R.drawable.remove,
 						getString(R.string.notification_dismiss), piDismiss)
 				.addAction(R.drawable.snooze,
 						getString(R.string.notification_snooze), piSnooze);
-				
-		return nb.build();
+		mNotificationManager.notify(NOTIFICATION_ID, builder.build());
+	}
+	
+	// creates a notification after another 5 minutes
+	private void startSnooze() {
+		PendingIntent notifyIntent = PendingIntent.getService(this, NOTIFICATION_ID_SNOOZE,
+				new Intent(this, NotificationService.class), 0);
+		AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+		long startTime = System.currentTimeMillis() + MILLIS;
+		am.set(AlarmManager.RTC_WAKEUP, startTime, notifyIntent);
+		Log.i(TAG, "Enabled notifications");
 	}
 
 }
